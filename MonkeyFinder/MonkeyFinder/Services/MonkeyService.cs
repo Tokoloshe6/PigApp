03099ba8a5328a3c81c56ef84a81191efdc2fc26
Joseph Warner <josephtoko77@gmail.com>
2024-05-21 00:15:16 +0100

    Added mobile app files

---
+++ MonkeyFinder/MonkeyFinder/Services/MonkeyService.cs
@@ -0,0 +1,22 @@
+﻿namespace MonkeyFinder.Services;
+using System.Net.Http.Json;
+public class MonkeyService
+{
+    List<Monkey> monkeyList = new();
+    HttpClient httpClient;
+
+    public async Task<List<Monkey>> GetMonkeys()
+    {
+        var response = await httpClient.GetAsync("https://raw.githubusercontent.com/tokoloshe6/PigApp/main/PigInfo.json");
+
+        if (response.IsSuccessStatusCode)
+        {
+            monkeyList = await response.Content.ReadFromJsonAsync(MonkeyContext.Default.ListMonkey);
+        }       
+        return monkeyList;
+    }
+    public MonkeyService()
+    {
+        this.httpClient = new HttpClient();
+    }
+}
